c5655f86ce1097300778fca9270edcca5362ed3c
sjgcit <sjgcit@gmail.com>
2014-12-08 13:51:46 +0000

    Create silly.c
    Demo source file for gccwrap demo.

---
+++ silly.c
@@ -0,0 +1,27 @@
+/*
+ * 
+ *
+ * $Id: silly.c,v 1.1 2014/12/02 02:06:02 sjg Exp $
+ *
+ * (c)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include "my.h"
+
+int main( int argc, char **argv )
+{
+    int retv = 0 ;
+    
+    myfunc() ;
+    
+    printf( "A silly test app\n" ) ;
+    
+    return retv ;
+}
+
+
