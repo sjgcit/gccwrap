9df3db2e909491031b571c0ec5dae66a6c0b2e6f
sjgcit <sjgcit@gmail.com>
2014-12-08 13:54:25 +0000

    Create my.h
    Part of the demo code.

---
+++ my.h
@@ -0,0 +1,16 @@
+
+/*
+ * Include file my.h
+ *
+ * $Id: my.h,v 1.1 2014/12/02 02:06:03 sjg Exp $
+ *
+ * (c) 
+ */
+
+
+#ifndef __MY_H
+#  define __MY_H
+
+extern void myfunc() ;
+
+#endif /* __MY_H */
